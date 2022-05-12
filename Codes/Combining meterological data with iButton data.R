9f7ee7b31ef820261a249352314c85b2e4b6a70b
GavinStark89 <104361337+GavinStark89@users.noreply.github.com>
2022-05-12 17:37:41 +0300

    Update Combining meterological data with iButton data.R

--- Codes/Combining meterological data with iButton data.R
+++ Codes/Combining meterological data with iButton data.R
@@ -11,7 +11,6 @@ library(MASS)
 library(mglmn)
 library(lubridate)
 library(MASS)
-library(lubridate)
 library(rayshader)
 library("lme4")
 library(MuMIn)
@@ -21,13 +20,11 @@ library(dplyr)
 library(Epi)
 library(statsr)
 library(dplyr)
-options(scipen = 999)
-meterological_files <- "C:/Users/gavin/Desktop/PhD/Chapters 2&3 - Modelling Lizard's Activity Time/new winter data for model 2022"
-setwd("C:/Users/gavin/Desktop/PhD/Chapters 2&3 - Modelling Lizard's Activity Time/new winter data for model 2022")
-#Winter:
-#Set columns for data and time separately:
 library(stringr)
 library(lubridate)
+options(scipen = 999)
+############Code can be applied for both summer and winter data (example below refer to winter data)############
+#Winter:
 #First: fixing the am-pm problem in the ibutton dataset:
 #Winter:
 winter_ibutton = read.csv("Full iButton Dataset.csv")
@@ -59,21 +56,6 @@ ibutton_meterological_winter <- merge(ibutton_data_winter, meterological_data_wi
 write.table(ibutton_meterological_winter, file="new_winter_microclimate_iButton.csv", row.names = F, col.names = T, sep=",")
 
 
-########################
-meterological_data_winter <- read.csv("CR300Series_MeteoData_01_12_2020.csv", sep=",", header = T)
-meterological_data_winter <- tidyr::separate(meterological_data_winter,TIMESTAMP, c("Date", "Time"), sep = " ",remove = FALSE)
-meterological_data_winter$dt <- ymd_hms(paste(meterological_data_winter$Date, meterological_data_winter$Time))
-meterological_data_winter$round_dt <- round_date(meterological_data_winter$dt, "30 minutes")
-meterological_data_winter <- tidyr::separate(meterological_data_winter,round_dt, c("Date", "Time"), sep = " ",remove = FALSE)
-
-#Load iButton data:
-ibutton_data_winter <- read.table("Full Thermal Data 02.08.2021.csv", sep=",", header = T)
-ibutton_data_winter <- tidyr::unite(ibutton_data_winter,Time_Date, c("Date", "Time"), sep = " ",remove = FALSE)
-ibutton_data_winter$dt <- dmy_hms(paste(ibutton_data_winter$Date, ibutton_data_winter$Time))
-ibutton_data_winter$round_dt = round_date(ibutton_data_winter$dt, "10 minutes")
-
-
-ibutton_meterological_winter <- merge(ibutton_data_winter, meterological_data_winter, by="round_dt")
 
 
 write.table(ibutton_meterological_winter, file="microclimate_fieldata.csv", row.names = F, col.names = T, sep=",")
