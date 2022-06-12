d412fa781f21b535dbb07250fe0baa766b6c5f11
GavinStark89 <104361337+GavinStark89@users.noreply.github.com>
2022-06-12 16:32:17 +0300

    Add files via upload

---
+++ Codes/Stats Analysis for temperatures under thermoregulatory shelters and open field.R
@@ -0,0 +1,44 @@
+library(lubridate)
+library(dplyr)
+library(tidyr)
+options(scipen = 999)
+setwd('C:/Users/gavin/Desktop/PhD/Chapter 2 -Microhabitat and Thermoregulation')
+Data <- read.csv(file.choose())
+#Create table of mean&min&max values for the raw table:
+Data <- tidyr::separate(Data, Time_Date, c("Date", "Time"), sep = " ",remove = FALSE) #separates the time and date column
+Max_ibutton_temp <- aggregate(iButton_Temp ~ Date+Object+Size, data = Data, max)#create maximum values according to object and size
+Min_ibutton_temp <- aggregate(iButton_Temp ~ Date+Object+Size, data = Data, min)#create minimum values according to object and size
+Mean_ibutton_temp <- aggregate(iButton_Temp ~ Date+Object+Size, data = Data, mean)#create average values according to object and size
+###stats for maximum temp under ground###
+library(dplyr)
+Max_ibutton_temp_bush <- Max_ibutton_temp %>%
+  group_by(Object) %>%
+  filter(!any(Object == "Rock"))
+Max_ibutton_temp_mean <- aggregate(Max_ibutton_temp_bush[, 4], list(Max_ibutton_temp_bush$Size), mean)
+Max_ibutton_temp_sd <- aggregate(Max_ibutton_temp_bush[, 4], list(Max_ibutton_temp_bush$Size), sd)
+
+###stats for minimum temp under ground###
+library(dplyr)
+Min_ibutton_temp_rock <- Min_ibutton_temp %>%
+  group_by(Object) %>%
+  filter(!any(Object == "Rock"))
+Min_ibutton_temp_mean <- aggregate(Min_ibutton_temp_rock[, 4], list(Min_ibutton_temp_rock$Size), mean)
+Min_ibutton_temp_sd <- aggregate(Min_ibutton_temp_rock[, 4], list(Min_ibutton_temp_rock$Size), sd)
+
+###stats for average temp under ground###
+library(dplyr)
+mean_ibutton_temp_rock <- Mean_ibutton_temp %>%
+  group_by(Object) %>%
+  filter(!any(Object == "Rock"))
+mean_ibutton_temp_mean <- aggregate(mean_ibutton_temp_rock[, 4], list(mean_ibutton_temp_rock$Size), mean)
+mean_ibutton_temp_sd <- aggregate(mean_ibutton_temp_rock[, 4], list(mean_ibutton_temp_rock$Size), sd)
+
+##Stats for open field temps
+Max_open_temp <- aggregate(iButton_Temp ~ Date+Object+Size, data = Data, max)#create maximum values according to object and size
+Min_open_temp <- aggregate(iButton_Temp ~ Date+Object+Size, data = Data, min)#create minimum values according to object and size
+Mean_open_temp <- aggregate(iButton_Temp ~ Date+Object+Size, data = Data, mean)
+
+
+
+
+
