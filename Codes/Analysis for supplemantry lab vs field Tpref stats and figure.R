de25ef9ad684b8efc5bac09c924cdb64207d15d6
GavinStark89 <104361337+GavinStark89@users.noreply.github.com>
2022-06-12 16:28:41 +0300

    Update and rename Analysis for supplemantry lab vs field Tpref stats.R to Analysis for supplemantry lab vs field Tpref stats and figure.R

--- Codes/Analysis for supplemantry lab vs field Tpref stats and figure.R
+++ Codes/Analysis for supplemantry lab vs field Tpref stats and figure.R
@@ -4,7 +4,7 @@ library(dplyr)
 library(nlme)
 options(scipen = 999)
 setwd('C:/Users/gavin/Desktop/PhD/Chapter 2 -Microhabitat and Thermoregulation/Temperature preference experiment/Thermal Pictures and Data of M. bahaeldini')
-Data <- read.csv(file.choose())
+Data <- read.csv()
 gls.model <- gls(Body_Temperature ~ Temp_Origin*Season, data = Data, weights = varIdent(form =~1|Temp_Origin*Season)) 
 summary(gls.model)
 
@@ -29,7 +29,7 @@ g1 <- g+theme_bw()+theme(axis.text=element_text(face='bold', size=15), axis.text
                          legend.text=element_text(size=20))+
   scale_fill_manual(values = c("Yellow1", "Yellow1","Blue","Blue"))+
   scale_x_discrete(name="Temperature origin|Season",labels=Labels_X)+
-  scale_y_continuous(name="Body temperature (°C)")
+  scale_y_continuous(name="Body temperature (Â°C)")
 g1
 g2 <- g1+theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
 g2
