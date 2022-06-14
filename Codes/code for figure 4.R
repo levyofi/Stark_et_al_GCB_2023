b1f2fe1505067cf850d8e80bd397d72156eeed9a
GavinStark89 <104361337+GavinStark89@users.noreply.github.com>
2022-06-14 12:20:35 +0300

    Update code for figure 4.R

--- Codes/code for figure 4.R
+++ Codes/code for figure 4.R
@@ -2,7 +2,7 @@ library(dplyr)
 library(plyr)
 ###Summer###
 setwd("C:/Users/gavin/Desktop/PhD/Chapter 2 -Microhabitat and Thermoregulation/Paper/New datasets for figures")
-Data <- read.csv("Summer_microhabitat_selection.csv")
+Data <- read.csv('Data/Summer_microhabitat_selection.csv')
 Data$Activity <-ifelse(Data$Active_location!="burrow", "yes", "no")
 Data$sumDE <- Data$mean_de*Data$n_hours
 
@@ -15,7 +15,7 @@ Data_summer$mean_activity_DE <- Data_summer$sum_DE/Data_summer$sum_hours
 Data_summer <- filter(Data_summer, Activity == "yes")
 Data_summer$activity_Percentage <- Data_summer$activity_percentage*100
 ###Figure###   activity_Percentage/mean_activity_DE
-tiff(file="Figure 5BSummer mean activity DE.tiff", width=3800, height=2500, res=300, compression="lzw")
+tiff(file="Figure 5ummer.tiff", width=3800, height=2500, res=300, compression="lzw")
 summer <- ggplot(Data_summer, aes(x=climate_change, y=mean_activity_DE))+
   geom_line(aes(group = habitat_loss, color=habitat_loss), size=2)
 summer
@@ -35,9 +35,9 @@ dev.off()
 
 ########################################################################################
 ###Winter###
-Data <- read.csv("Winter_microhabitat_selection.csv")
-Data$Activity <-ifelse(Data$Active_location!="burrow", "yes", "no")
-Data$sumDE <- Data$mean_de*Data$n_hours
+Data1 <- read.csv('Data/Winter_microhabitat_selection.csv')
+Data1$Activity <-ifelse(Data1$Active_location!="burrow", "yes", "no")
+Data1$sumDE <- Data1$mean_de*Data1$n_hours
 
 Data_winter <- ddply(Data, .(habitat_loss,climate_change,
                              Activity), summarise, 
@@ -48,7 +48,7 @@ Data_winter$mean_activity_DE <- Data_winter$sum_DE/Data_winter$sum_hours
 Data_winter <- filter(Data_winter, Activity == "yes")
 Data_winter$activity_Percentage <- Data_winter$activity_percentage*100
 ###Figure###
-tiff(file="Figure 5Dwinter mean activity DE.tiff", width=3800, height=2500, res=300, compression="lzw")
+tiff(file="Figure 5 winter.tiff", width=3800, height=2500, res=300, compression="lzw")
 winter <- ggplot(Data_winter, aes(x=climate_change, y=mean_activity_DE))+
   geom_line(aes(group = habitat_loss, color=habitat_loss), size=2)
 winter
